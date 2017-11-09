9d09cd29f537703de1adb34204e612e34da7f6f5
Robert Carpenter <rjctlc@aol.com>
2017-11-09 13:28:05 -0500

    compiled exe

---
+++ ConsoleApplication1/AppleResponse.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+
+    public class AppleResponse
+    {
+        public Head head { get; set; }
+        public Body body { get; set; }
+    }
+
+    public class Head
+    {
+        public string status { get; set; }
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+    }
+
+    public class Body
+    {
+        public Store[] stores { get; set; }
+        public bool overlayInitiatedFromWarmStart { get; set; }
+        public string viewMoreHoursLinkText { get; set; }
+        public string storesCount { get; set; }
+        public bool little { get; set; }
+        public string location { get; set; }
+        public string notAvailableNearby { get; set; }
+        public string notAvailableNearOneStore { get; set; }
+        public string productLocatorSpecialHours { get; set; }
+        public string productLocatorSpecialHoursView { get; set; }
+    }
+
+    public class Store
+    {
+        public string storeEmail { get; set; }
+        public string storeName { get; set; }
+        public string reservationUrl { get; set; }
+        public string makeReservationUrl { get; set; }
+        public string state { get; set; }
+        public string storeImageUrl { get; set; }
+        public string country { get; set; }
+        public string city { get; set; }
+        public string storeNumber { get; set; }
+        public Dictionary<string, Parts> partsAvailability { get; set; }
+        public string phoneNumber { get; set; }
+        public Address address { get; set; }
+        public string hoursUrl { get; set; }
+        public string directionsUrl { get; set; }
+        public Storehours storeHours { get; set; }
+        public float storelatitude { get; set; }
+        public float storelongitude { get; set; }
+        public float storedistance { get; set; }
+        public string storeDistanceWithUnit { get; set; }
+        public string storeDistanceVoText { get; set; }
+        public int storelistnumber { get; set; }
+        public int storeListNumber { get; set; }
+    }
+
+    public class Partsavailability
+    {
+        public MQAM2LLA MQAM2LLA { get; set; }
+    }
+
+
+    public class Parts
+    {
+        public bool storePickEligible { get; set; }
+        public bool storeSearchEnabled { get; set; }
+        public bool storeSelectionEnabled { get; set; }
+        public string storePickupQuote { get; set; }
+        public string pickupSearchQuote { get; set; }
+        public string storePickupLabel { get; set; }
+        public string partNumber { get; set; }
+        public string purchaseOption { get; set; }
+        public string ctoOptions { get; set; }
+        public string storePickupLinkText { get; set; }
+        public string storePickupProductTitle { get; set; }
+        public string pickupDisplay { get; set; }
+    }
+
+
+    public class MQAM2LLA
+    {
+        public bool storePickEligible { get; set; }
+        public bool storeSearchEnabled { get; set; }
+        public bool storeSelectionEnabled { get; set; }
+        public string storePickupQuote { get; set; }
+        public string pickupSearchQuote { get; set; }
+        public string storePickupLabel { get; set; }
+        public string partNumber { get; set; }
+        public string purchaseOption { get; set; }
+        public string ctoOptions { get; set; }
+        public string storePickupLinkText { get; set; }
+        public string storePickupProductTitle { get; set; }
+        public string pickupDisplay { get; set; }
+    }
+
+    public class Address
+    {
+        public string address { get; set; }
+        public object address3 { get; set; }
+        public string address2 { get; set; }
+        public string postalCode { get; set; }
+    }
+
+    public class Storehours
+    {
+        public string storeHoursText { get; set; }
+        public string bopisPickupDays { get; set; }
+        public string bopisPickupHours { get; set; }
+        public Hour[] hours { get; set; }
+    }
+
+    public class Hour
+    {
+        public string storeTimings { get; set; }
+        public string storeDays { get; set; }
+    }
+
+
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,49 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            string zipCode = "";
+
+            Console.WriteLine("Please enter your Zip Code that you want to search around.");
+
+            zipCode = Console.ReadLine();
+            while (true)
+            {
+                //CheckForIPhoneX(zipCode, "ATT/US");
+                CheckForIPhoneX(zipCode, "TMOBILE/US");
+                //CheckForIPhoneX(zipCode, "VERIZON/US");
+                //CheckForIPhoneX(zipCode, "SPRINT/US");
+            }
+        }
+
+        private static void CheckForIPhoneX(string zipCode, string carrier)
+        {
+            try
+            {
+                var client = new WebClient();
+                //https://www.apple.com/shop/retail/pickup-message?pl=true&cppart=TMOBILE/US&parts.0=MQAM2LL/A&location=15106
+                //https://www.apple.com/shop/retail/pickup-message?pl=true&cppart=TMOBILE/US&parts.0=MQAM2LL/A&location={zipCode}
+                var jsonString = client.DownloadString($@"https://www.apple.com/shop/retail/pickup-message?pl=true&cppart={carrier}&parts.0=MQAU2LL/A&parts.1=MQAQ2LL/A&parts.2=MQAR2LL/A&parts.3=MQAV2LL/A&location={zipCode}");
+
+                var response = JsonConvert.DeserializeObject<AppleResponse>(jsonString);
+
+                if (response.body.stores.Any(x => x.partsAvailability != null && x.partsAvailability.Count > 0))
+                {
+
+                    foreach (var store in response.body.stores.Where(x => x.partsAvailability != null && x.partsAvailability.Count > 0))
+                    {
+                        foreach (var part in store.partsAvailability.Where(x => x.Value.pickupDisplay != "unavailable"))
+                        {
+                            Console.WriteLine($"{carrier}: Phone {part.Value.storePickupProductTitle} found at {store.storeName}");
+                        }
+
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
